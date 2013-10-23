706a7f2b271cd35843e42872bbfe8391e4848f48
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-10-23 23:11:45 +0100

    Initial commit containing starting ini.pp file.
    Hope this works.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Colloquy.app into /Applications.
+#
+# Examples
+#
+#   include colloquy
+class colloquy {
+  package { 'Colloquy':
+    provider   => 'compressed_app',
+    source     => 'http://colloquy.info/downloads/colloquy-latest.zip'
+  }
 }
